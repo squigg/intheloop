148c4dd9dd3b274e8bdbf7c5f99c47f7b0325493
Squigg <github@squigg.co.uk>
2016-11-08 19:27:08 +0000

    update mongo config

--- services/mongo.js
+++ services/mongo.js
@@ -2,15 +2,10 @@
  * Created by squigg on 08/11/16.
  */
 const mongo = require("mongodb").MongoClient;
-const mongoConfig = {
-    url: process.env.mongoServer || "localhost",
-    port: process.env.mongoPort || "27017",
-    db: process.env.mongoDB || "intheloop"};
+const mongoConnect = process.env.mongoConnect || "mongodb://localhost:10250/intheloop";
 
 exports.withMongo = function (callback) {
-    var url = "mongodb://" + mongoConfig.url + ":" + mongoConfig.port + "/" + mongoConfig.db;
-    mongo.connect(url, function (err, db) {
-        console.log(err);
+    mongo.connect(mongoConnect, function (err, db) {
         callback(db);
         db.close();
     });
