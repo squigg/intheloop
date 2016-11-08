7fff40f79ed919563c27b41a2a38175aac2b7aa1
Squigg <github@squigg.co.uk>
2016-11-08 19:23:48 +0000

    changed again

--- services/mongo.js
+++ services/mongo.js
@@ -2,7 +2,10 @@
  * Created by squigg on 08/11/16.
  */
 const mongo = require("mongodb").MongoClient;
-const mongoConfig = { url: "localhost", port: "27017", db: "intheloop"};
+const mongoConfig = {
+    url: process.env.mongoServer || "localhost",
+    port: process.env.mongoPort || "27017",
+    db: process.env.mongoDB || "intheloop"};
 
 exports.withMongo = function (callback) {
     var url = "mongodb://" + mongoConfig.url + ":" + mongoConfig.port + "/" + mongoConfig.db;
